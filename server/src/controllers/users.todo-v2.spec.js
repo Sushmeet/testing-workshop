00ae54766a0fd5f74b197533040d161d1f561ba3
sushmeet sunger <ssunger@shutterstock.com>
2020-06-06 15:47:20 -0400

    user-todo-test

---
+++ server/src/controllers/users.todo-v2.spec.js
@@ -0,0 +1,23 @@
+import {initDb} from 'til-server-test-utils'
+import {omit} from 'lodash'
+import * as userController from './users'
+
+describe('Getusers', () => {
+  test('getUsers returns all the users in the database', async () => {
+    const {users: actualUsers} = await initDb()
+    const safeUser = user => {
+      return omit(user, ['exp', 'iat', 'hash', 'salt'])
+    }
+
+    const req = {}
+    const res = {
+      json: jest.fn(),
+    }
+
+    await userController.getUsers(req, res)
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({
+      users: actualUsers.map(safeUser),
+    })
+  })
+})
